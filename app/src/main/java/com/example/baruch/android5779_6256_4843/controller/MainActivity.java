1206303db872860c33c41dea9a0056c73e0a5ec0
gehlerb <gehlerb@gmail.com>
2018-11-04 21:07:40 +0200

    minor improvement OrderRideActivity UI

--- app/src/main/java/com/example/baruch/android5779_6256_4843/controller/MainActivity.java
+++ app/src/main/java/com/example/baruch/android5779_6256_4843/controller/MainActivity.java
@@ -1,15 +1,32 @@
 package com.example.baruch.android5779_6256_4843.controller;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.baruch.android5779_6256_4843.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button startButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startButton=(Button)findViewById(R.id.startButton);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openOrderRideActivity();
+            }
+        });
+    }
+
+    public void openOrderRideActivity(){
+        Intent intent=new Intent(this,OrderRideActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/baruch/android5779_6256_4843/controller/OrderRideActivity.java
+++ app/src/main/java/com/example/baruch/android5779_6256_4843/controller/OrderRideActivity.java
@@ -1,7 +1,9 @@
-package com.example.baruch.android5779_6256_4843;
+package com.example.baruch.android5779_6256_4843.controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.baruch.android5779_6256_4843.R;
 
 public class OrderRideActivity extends AppCompatActivity {
 
