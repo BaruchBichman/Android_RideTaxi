4e124f6f1cee1f11ed9b3781f484412917893ecb
gehlerb <gehlerb@gmail.com>
2018-11-08 23:07:47 +0200

    just started

--- app/src/main/java/com/example/baruch/android5779_6256_4843/controller/OrderRideActivity.java
+++ app/src/main/java/com/example/baruch/android5779_6256_4843/controller/OrderRideActivity.java
@@ -1,25 +1,14 @@
 package com.example.baruch.android5779_6256_4843.controller;
 
 
-import android.Manifest;
-import android.annotation.SuppressLint;
-import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.Address;
-import android.location.Geocoder;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.os.Build;
-import android.support.v7.app.AppCompatActivity;
+import android.content.IntentSender;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.example.baruch.android5779_6256_4843.R;
@@ -27,16 +16,20 @@ import com.example.baruch.android5779_6256_4843.model.backend.Backend;
 import com.example.baruch.android5779_6256_4843.model.backend.BackendFactory;
 import com.example.baruch.android5779_6256_4843.model.entities.ClientRequestStatus;
 import com.example.baruch.android5779_6256_4843.model.entities.Ride;
-
-import java.io.IOException;
-import java.util.List;
-import java.util.Locale;
-
-
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.SettingsClient;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.ui.PlacePicker;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 import static android.widget.Toast.LENGTH_LONG;
 
@@ -44,6 +37,7 @@ public class OrderRideActivity extends AppCompatActivity{
 
     final int PLACE_PICKER_REQUEST_DESTINATION = 1;
     final int PLACE_PICKER_REQUEST_PICKUP = 2;
+    final int REQUEST_CHECK_SETTINGS=3;
     private Button newRideButton;
     private EditText firstNameEditText;
     private EditText lastNameEditText;
@@ -53,11 +47,9 @@ public class OrderRideActivity extends AppCompatActivity{
     private EditText pickUpAddressEditText;
     private Ride ride;
 
+    private FusedLocationProviderClient mFusedLocationClient;
+    private LocationRequest mLocationRequest;
     private static Backend backend;
-    // Acquire a reference to the system Location Manager
-    private LocationManager locationManager;
-    // Define a listener that responds to location updates
-    private LocationListener locationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,6 +58,50 @@ public class OrderRideActivity extends AppCompatActivity{
 
         ride=new Ride();
         backend=BackendFactory.getBackend();
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        createLocationRequest();
+        findViews();
+        getMyLocation();
+    }
+
+    private void getMyLocation() {
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(mLocationRequest);
+        SettingsClient client = LocationServices.getSettingsClient(this);
+        Task<LocationSettingsResponse> task = client.checkLocationSettings(builder.build());
+        task.addOnSuccessListener(this, new OnSuccessListener<LocationSettingsResponse>() {
+            @Override
+            public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+
+            }
+        }).addOnFailureListener(this, new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                if (e instanceof ResolvableApiException) {
+                    // Location settings are not satisfied, but this can be fixed
+                    // by showing the user a dialog.
+                    try {
+                        // Show the dialog by calling startResolutionForResult(),
+                        // and check the result in onActivityResult().
+                        ResolvableApiException resolvable = (ResolvableApiException) e;
+                        resolvable.startResolutionForResult(OrderRideActivity.this,
+                                REQUEST_CHECK_SETTINGS);
+                    } catch (IntentSender.SendIntentException sendEx) {
+                        // Ignore the error.
+                    }
+                }
+            }
+        });
+
+    }
+
+    private void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+    private void findViews() {
         newRideButton = (Button) findViewById(R.id.orderTaxiButton);
         firstNameEditText = (EditText) findViewById(R.id.firstNameEditText);
         lastNameEditText = (EditText) findViewById(R.id.lastNameEditText);
@@ -100,41 +136,25 @@ public class OrderRideActivity extends AppCompatActivity{
                 }
             }
         });
-        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-
-        // Define a listener that responds to location updates
-        locationListener = new LocationListener() {
-            public void onLocationChanged(Location location) {
-                pickUpAddressEditText.setText(getPlace(location));
-            }
-
-            public void onStatusChanged(String provider, int status, Bundle extras) {
-            }
-
-            public void onProviderEnabled(String provider) {
-            }
-
-            public void onProviderDisabled(String provider) {
-            }
-        };
 
         newRideButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ride.setClientFirstName(firstNameEditText.getText().toString());
-                ride.setClientLastName(lastNameEditText.getText().toString());
-                ride.setClientEmail(emailEditText.getText().toString());
-                ride.setClientTelephone(phoneNumberEditText.getText().toString());
-                ride.setDestinationAddress(destinationAddressEditText.getText().toString());
-                ride.setPickupAddress(pickUpAddressEditText.getText().toString());
-                ride.setRideState(ClientRequestStatus.WAITING);
+                setRide();
                 addClientRequestToDataBase(ride);
             }
         });
-
-        getLocation();
     }
 
+    private void setRide() {
+        ride.setClientFirstName(firstNameEditText.getText().toString());
+        ride.setClientLastName(lastNameEditText.getText().toString());
+        ride.setClientEmail(emailEditText.getText().toString());
+        ride.setClientTelephone(phoneNumberEditText.getText().toString());
+        ride.setDestinationAddress(destinationAddressEditText.getText().toString());
+        ride.setPickupAddress(pickUpAddressEditText.getText().toString());
+        ride.setRideState(ClientRequestStatus.WAITING);
+    }
 
     private void addClientRequestToDataBase(Ride ride) {
         newRideButton.setEnabled(false);
@@ -152,50 +172,6 @@ public class OrderRideActivity extends AppCompatActivity{
 
     }
 
-    private void getLocation() {
-        //     Check the SDK version and whether the permission is already granted or not.
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 5);
-        } else {
-            // Android version is lesser than 6.0 or the permission is already granted.
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
-        }
-    }
-
-    public String getPlace(Location location) {
-
-        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
-        List<Address> addresses = null;
-        try {
-            addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
-
-            if (addresses.size() > 0) {
-                String address = addresses.get(0).getAddressLine(0);
-                return address;
-            } else {
-                return "no place: \n (" + location.getLongitude() + " , " + location.getLatitude() + ")";
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return "IOException ...";
-    }
-
-    @SuppressLint("MissingPermission")
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        if (requestCode == 5) {
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // Permission is granted
-                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
-
-            } else {
-                Toast.makeText(this, "Until you grant the permission, we canot display the location", Toast.LENGTH_SHORT).show();
-            }
-        }
-
-    }
-
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == PLACE_PICKER_REQUEST_DESTINATION) {
             if (resultCode == RESULT_OK) {
@@ -205,7 +181,6 @@ public class OrderRideActivity extends AppCompatActivity{
         }
         if (requestCode == PLACE_PICKER_REQUEST_PICKUP) {
             if (resultCode == RESULT_OK) {
-                locationManager.removeUpdates(locationListener);
                 Place place = PlacePicker.getPlace(data, this);
                 pickUpAddressEditText.setText(place.getAddress());
             }
